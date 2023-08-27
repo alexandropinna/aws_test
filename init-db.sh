baa894c5f057bfa6099725108124d3e40fbfd66c
Alejandro Piña <alex919090@gmail.com>
2023-08-26 20:08:06 -0400

    cambio de comando para ejecutar script

--- init-db.sh
+++ init-db.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Comandos para conectarse al RDS y crear la base de datos
-psql -h $RDS_ENDPOINT -U $RDS_USERNAME -d $RDS_DBNAME -c "CREATE DATABASE testing_db_ecs;"
+#psql -h $RDS_ENDPOINT -U $RDS_USERNAME -d $RDS_DBNAME -c "CREATE DATABASE testing_db_ecs;"
+psql "host=$DB_HOST port=$DB_PORT dbname=postgres user=$DB_USER password=$DB_PASS" -c "CREATE DATABASE testing_db_ecs;"
