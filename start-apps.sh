6ee80c4cef35c751398398368b00e32d21eb35b8
Alejandro Piña <alex919090@gmail.com>
2023-08-27 00:04:12 -0400

    cambios para ejecutar archivo de .NET

--- start-apps.sh
+++ start-apps.sh
@@ -5,7 +5,7 @@ java -jar /usr/src/app/java_project/target/helloworld-1.0-SNAPSHOT.jar &
 java -jar /usr/src/app/maven_project/target/helloworld-maven-1.0-SNAPSHOT.jar &
 
 # Iniciar la aplicación .NET en el puerto 83
-dotnet /usr/src/app/net_project/bin/Release/net5.0/net_project.dll &
+dotnet /app/HelloWorld.dll &
 
 # Mantener el contenedor en ejecución
 tail -f /dev/null
