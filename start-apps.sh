e0232ee9bbe33c212d0fe42a7858bf6175afe3eb
Alejandro Piña <alex919090@gmail.com>
2023-08-27 01:18:48 -0400

    cambios para ejecutar archivos

--- start-apps.sh
+++ start-apps.sh
@@ -1,15 +1,29 @@
-# Iniciar el servidor Java en el puerto 81
+# Este script inicia varios servicios, incluidos servidores Java, Maven, .NET, Apache y PostgreSQL.
+
+# --- Inicio de Servicios Individuales ---
+
+# Iniciar el servidor Java
+# Se ejecuta en segundo plano y escucha en el puerto 81
 java -jar /usr/src/app/java_project/target/helloworld-1.0-SNAPSHOT.jar &
 
-# Iniciar el servidor Maven en el puerto 82
+# Iniciar el servidor Maven
+# Se ejecuta en segundo plano y escucha en el puerto 82
 java -jar /usr/src/app/maven_project/target/helloworld-maven-1.0-SNAPSHOT-jar-with-dependencies.jar &
 
-# Iniciar la aplicación .NET en el puerto 83
+# Iniciar la aplicación .NET
+# Se ejecuta en segundo plano y escucha en el puerto 83
 dotnet /app/dotnet_project/HelloWorld.dll &
 
+# Iniciar el servidor web Apache
+# Se ejecuta en segundo plano con la configuración predeterminada
 apachectl -D FOREGROUND &
 
-service postgresql start
+# Iniciar el servidor de base de datos PostgreSQL
+# Se ejecuta en segundo plano con la configuración predeterminada
+service postgresql start &
+
+# --- Mantener el Contenedor en Ejecución ---
 
 # Mantener el contenedor en ejecución
+# 'tail -f /dev/null' es una forma común de mantener un contenedor Docker en ejecución
 tail -f /dev/null
